aa051efd0b73cf1029b507b6d0234acd72b490db
wademegan96@gmail.com <meganwade@Megans-MacBook-Pro.local>
2020-03-16 14:09:33 -0500

    updated

--- src/requests/requests-service.js
+++ src/requests/requests-service.js
@@ -28,7 +28,7 @@ const RequestsService={
                 'users.first_name AS first_name',
                 'users.last_name AS last_name'
             )
-            .join('brand_users AS users','users.id','req.user_id')
+            .join('brand_users AS users','req.user_id','users.id')
             .where('brand_requests.id',id)
             .then(([request])=>request)
             //.returning('*')
