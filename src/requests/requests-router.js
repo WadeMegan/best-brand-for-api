cb8bd51c34f59d3e6bd8a778fe16f881855850e8
wademegan96@gmail.com <meganwade@Megans-MacBook-Pro.local>
2020-03-16 13:40:21 -0500

    updated

--- src/requests/requests-router.js
+++ src/requests/requests-router.js
@@ -46,8 +46,8 @@ requestsRouter
             req.app.get('db'),
             req.params.request_id
         )
-            .then(request=>{
-                res.json(RequestsService.serializeRequest(request))
+            .then(requests=>{
+                res.json(requests.map(RequestsService.serializeRequest))
             })
             .catch(next)
     })
